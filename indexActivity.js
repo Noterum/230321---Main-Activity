6007e0ed62847112e4e4867beb38cac65c8be937
Johann Michael B. Caayao <micobeecee3@gmail.com>
2023-03-21 09:09:05 +0800

    initial commit

---
+++ indexActivity.js
@@ -0,0 +1,12 @@
+console.log('Hello, Welcome to De La Salle University!');
+
+let homeAddress1 = {houseNumber: "B4 L3", street: "Ma. Aurora Avenue", village: "Ma. Aurora Heights Subdivision", barangay: "San Antonio", city: "San Pedro", province: "Laguna", zipCode: "4023"};
+
+let student1 = {
+	firstName: "Johann Michael",
+	lastName: "Caayao",
+	age: 20,
+	hobbies: ['Gaming', 'Bowling', 'Volleyball', 'Basketball', 'Card Collector'],
+	homeAddress: homeAddress1
+};
+console.log(student1);
